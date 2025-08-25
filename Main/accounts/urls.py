a91d8f99997da1133dd34eea5f3fca555979983f
Mfonobong Uyah <97167651+investor-uyah@users.noreply.github.com>
2025-08-25 14:26:12 +0100

    standardizing url patterns

--- Main/accounts/urls.py
+++ Main/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .. import views
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
 ]
