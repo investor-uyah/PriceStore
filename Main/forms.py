edc96a7c0577bea40c881fef328f895d13d337f2
Mfonobong Uyah <97167651+investor-uyah@users.noreply.github.com>
2025-09-03 00:48:35 +0100

    updated list of foodstuffs for choices

--- Main/forms.py
+++ Main/forms.py
@@ -12,30 +12,33 @@ TOPIC_CHOICES = (
 
 # A list of Nigerian states and their acronyms (or full names)
 STATES_CHOICES = [
-    ('ABIA', 'Abia'), ('ADAMAWA', 'Adamawa'), ('AKWA-IBOM', 'Akwa Ibom'),
-    ('ANAMBRA', 'Anambra'), ('BAUCHI', 'Bauchi'), ('BAYELSA', 'Bayelsa'),
-    ('BENUE', 'Benue'), ('BORNO', 'Borno'), ('CROSS-RIVER', 'Cross River'),
-    ('DELTA', 'Delta'), ('EBONYI', 'Ebonyi'), ('EDO', 'Edo'),
-    ('EKITI', 'Ekiti'), ('ENUGU', 'Enugu'), ('GOMBE', 'Gombe'),
-    ('IMO', 'Imo'), ('JIGAWA', 'Jigawa'), ('KADUNA', 'Kaduna'),
-    ('KANO', 'Kano'), ('KATSINA', 'Katsina'), ('KEBBI', 'Kebbi'),
-    ('KOGI', 'Kogi'), ('KWARA', 'Kwara'), ('LAGOS', 'Lagos'),
-    ('NASARAWA', 'Nasarawa'), ('NIGER', 'Niger'), ('OGUN', 'Ogun'),
-    ('ONDO', 'Ondo'), ('OSUN', 'Osun'), ('OYO', 'Oyo'),
-    ('PLATEAU', 'Plateau'), ('RIVERS', 'Rivers'), ('SOKOTO', 'Sokoto'),
-    ('TARABA', 'Taraba'), ('YOBE', 'Yobe'), ('ZAMFARA', 'Zamfara'),
-    ('FCT', 'Federal Capital Territory')
+    ('', 'Choose a state...'),
+    ('FCT', 'FCT'),
+    ('Abia', 'Abia'), ('Adamawa', 'Adamawa'), ('Akwa Ibom', 'Akwa Ibom'),
+    ('Anambra', 'Anambra'), ('Bauchi', 'Bauchi'), ('Bayelsa', 'Bayelsa'),
+    ('Benue', 'Benue'), ('Borno', 'Borno'), ('Cross River', 'Cross River'),
+    ('Delta', 'Delta'), ('Ebonyi', 'Ebonyi'), ('Edo', 'Edo'),
+    ('Ekiti', 'Ekiti'), ('Enugu', 'Enugu'), ('Gombe', 'Gombe'),
+    ('Imo', 'Imo'), ('Jigawa', 'Jigawa'), ('Kaduna', 'Kaduna'),
+    ('Kano', 'Kano'), ('Katsina', 'Katsina'), ('Kebbi', 'Kebbi'),
+    ('Kogi', 'Kogi'), ('Kwara', 'Kwara'), ('Lagos', 'Lagos'),
+    ('Nasarawa', 'Nasarawa'), ('Niger', 'Niger'), ('Ogun', 'Ogun'),
+    ('Ondo', 'Ondo'), ('Osun', 'Osun'), ('Oyo', 'Oyo'),
+    ('Plateau', 'Plateau'), ('Rivers', 'Rivers'), ('Sokoto', 'Sokoto'),
+    ('Taraba', 'Taraba'), ('Yobe', 'Yobe'), ('Zamfara', 'Zamfara'),
 ]
 
 # A list of common foodstuffs to prevent invalid entries
 FOODSTUFFS_CHOICES = [
-    ('RICE', 'Rice'), ('BEANS', 'Beans'), ('YAM', 'Yam'),
-    ('GARRI', 'Garri'), ('PLANTAIN', 'Plantain'), ('CASSAVA', 'Cassava'),
-    ('WHEAT', 'Wheat'), ('MAIZE', 'Maize'), ('VEGETABLES', 'Vegetables'),
-    ('TOMATOES', 'Tomatoes'), ('PEPPER', 'Pepper'), ('ONIONS', 'Onions'),
-    ('FISH', 'Fish'), ('MEAT', 'Meat'), ('CHICKEN', 'Chicken'),
-    ('EGGS', 'Eggs'), ('SALT', 'Salt'), ('SUGAR', 'Sugar'),
-    ('OIL', 'Oil'), ('MILK', 'Milk'),
+    ('', 'Choose a foodstuff...'), ('Beans (brown)', 'Beans (brown)'), ('Beans (white)', 'Beans (white)'),
+    ('Cassava', 'Cassava'), ('Chicken', 'Chicken'), ('Eggs', 'Eggs'), ('Fish (crayfish)', 'Fish (crayfish)'),
+    ('Fish (fresh fish)', 'Fish (fresh fish)'), ('Fish (stockfish)', 'Fish (stockfish)'), ('Garri', 'Garri'),
+    ('Maize', 'Maize'), ('Meat (beef)', 'Meat (beef)'), ('Meat (goat)', 'Meat (goat)'), ('Milk', 'Milk'), 
+    ('Oil (palm oil)', 'Oil (palm oil)'), ('Oil (vegetable oil)', 'Oil (vegetable oil)'), ('Onions', 'Onions'),
+    ('Pepper', 'Pepper'), ('Plantain', 'Plantain'), ('Potato (irish)', 'Potato (irish)'),
+    ('Potato (sweet)', 'Potato (sweet)'), ('Poundo', 'Poundo'), ('Rice (foreign)', 'Rice (foreign)'),
+    ('Rice (local)', 'Rice (local)'), ('Salt', 'Salt'), ('Sugar', 'Sugar'),
+    ('Tomatoes', 'Tomatoes'), ('Turkey', 'Turkey'), ('Vegetables', 'Vegetables'), ('Wheat', 'Wheat'), ('Yam', 'Yam'),
 ]
 
 class ContactForm(forms.Form):
