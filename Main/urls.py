817398d9624b9a42a3ca6d70c21b735e65fef649
Mfonobong Uyah <97167651+investor-uyah@users.noreply.github.com>
2025-09-02 16:38:16 +0100

    removed food_count url

--- Main/urls.py
+++ Main/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('purchaseupdate/', views.purchase, name="purchase"),
     path('prices/', views.prices_combined, name="prices"),
     path('prices/states/<str:state>/', views.states_listing, name='states_listing'),
-    path('food-count/', views.food_count, name='food_count'),
     path('members/details/<int:id>/', views.details, name="details"),
     path('contact/', views.contact, name="contact"),
     path('about/', views.about, name="about"),
