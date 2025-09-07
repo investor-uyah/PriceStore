57ad56e9de21d6968cc8c1fd9be84e9f4a6b38e4
Mfonobong Uyah <investor.uyah@gmail.com>
2025-09-07 07:27:13 +0100

    (chore): add cache db and necessary packages

--- PriceStore/settings.py
+++ PriceStore/settings.py
@@ -102,6 +102,18 @@ DATABASES = {
     'default': dj_database_url.parse(database_url_value)
 }
 
+# Cache setup
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
