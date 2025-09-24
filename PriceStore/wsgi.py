9353ec2728dddf6fbb398dfe8f0435c1a7ed22fa
Mfonobong Uyah <investor.uyah@gmail.com>
2025-09-24 14:38:57 +0100

    (fix): Add required variable name 'app'

--- PriceStore/wsgi.py
+++ PriceStore/wsgi.py
@@ -17,4 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PriceStore.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+app = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
