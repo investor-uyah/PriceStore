5e74dafada84227d348a46fd477854d7d8f7b3ee
Mfonobong Uyah <investor.uyah@gmail.com>
2025-09-24 15:01:08 +0100

    (fix): Correct pointer to staticfiles directory

--- PriceStore/wsgi.py
+++ PriceStore/wsgi.py
@@ -17,4 +17,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PriceStore.settings')
 
 application = get_wsgi_application()
 
-app = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+app = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles'))
