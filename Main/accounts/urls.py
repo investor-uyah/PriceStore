4ce5138bebc7d93f4b429a2969e24818ce4d1716
Mfonobong Uyah <investor.uyah@gmail.com>
2026-01-13 15:28:02 +0100

    (fix): Correct logout url name

--- Main/accounts/urls.py
+++ Main/accounts/urls.py
@@ -5,6 +5,6 @@ from .. import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
-    path('logout/', LogoutView.as_view(), name='logout_new')
+    path('logout/', LogoutView.as_view(), name='logout')
 
 ]
