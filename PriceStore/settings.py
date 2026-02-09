0941b7f1820c1cbde85c10100f425525d7197e35
Mfonobong Uyah <investor.uyah@gmail.com>
2026-02-09 14:57:43 +0100

    (fix): Allow package install during Vercel build

--- PriceStore/settings.py
+++ PriceStore/settings.py
@@ -164,6 +164,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = '/static/'
 
 # Where collected static files will go (used in production)
--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt
+
+# Allows installing packages without virtualenv, which is required for the build process
+python3 -m pip install -r requirements.txt --break-system-packages
+
+# Collect static files for production
 python3 manage.py collectstatic --noinput
